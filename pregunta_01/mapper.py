d44b872b60d5dc552787956cb30acec8071e0997
anlopezma <126220614+anlopezma@users.noreply.github.com>
2023-06-10 16:26:47 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,7 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+ for line in sys.stdin:
+    credit_history = line.split(',')[2]
+    sys.stdout.write("{}\t1\n".format(credit_history))
