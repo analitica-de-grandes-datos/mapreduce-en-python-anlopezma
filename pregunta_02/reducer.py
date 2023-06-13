2e087620438d4d5c2c81426b5f43232fb4b5ee35
anlopezma <126220614+anlopezma@users.noreply.github.com>
2023-06-13 17:35:04 -0500

    Update reducer.py

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+max_amounts = {}
+
+for line in sys.stdin: 
+    line = line.strip()
+    purpose, amount = line.split("\t",1)
+    amount = int (amount)
+
+    if purpose in max_amounts:
+       max_amounts[purpose]= max(max_amounts[purpose], amount)
+    else:
+      max_amounts[purpose] = amount
+
+for purpose, max_amount in max_amounts.items():
+    print(f'{purpose}\t{max_amount}')
