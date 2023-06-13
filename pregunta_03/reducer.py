bc9913738a46d12b9391ce48e7e429c34a8e2c33
anlopezma <126220614+anlopezma@users.noreply.github.com>
2023-06-13 17:54:52 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+lines = sys.stdin.readlines()
+
+sorted_lines= sorted(lines, key=lambda x: x.split(",")[1])
+
+for line in sorted_lines:
+  print(line.strip())
