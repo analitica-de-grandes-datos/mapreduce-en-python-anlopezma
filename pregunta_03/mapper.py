a141cc732efdfc0c3d9451ac286f81e6dc0065e6
anlopezma <126220614+anlopezma@users.noreply.github.com>
2023-06-13 17:54:31 -0500

    Update mapper.py

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+import csv
+
+for line in sys.stdin:
+    columns = line.split().split(",")
+
+   if len(columns) >=2:
+
+        key= columns[1]
+        value = line.strip()
+
+        print(f"{key}\t{value}")
+
