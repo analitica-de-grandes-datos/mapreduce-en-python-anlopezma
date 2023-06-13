68dd3861266c721a5579ffe7afb4e5d5899e56b2
anlopezma <126220614+anlopezma@users.noreply.github.com>
2023-06-13 17:34:36 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+import csv
+
+for row in sys.stdin:
+    elementos = row.split(',')
+
+    amount= elementos[4]
+    purpose= elementos[3]
+    
+    linea= purpose + ";"+ amount
+    sys.stdout.write(linea+"\n")
