186d2ba0ecfa186663ede254aeb1884219372b57
anlopezma <126220614+anlopezma@users.noreply.github.com>
2023-06-13 21:02:08 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+with open('data.csv', 'r') as archivocsv:
+    for line in archivocsv:
+        line = line.strip()
+        columns = line.split("\t")
+
+        if len(columns) >= 2:
+            letters = columns[1].split(",")  
+            number = columns[0]
+
+            for letter in letters:
+                print(f'{letter}\t{number}')
