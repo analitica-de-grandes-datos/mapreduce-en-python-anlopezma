f4a978efe299da07580638bbbd6a930d6590881d
anlopezma <126220614+anlopezma@users.noreply.github.com>
2023-06-13 20:39:19 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+with open('data.csv', 'r') as archivocsv:
+    for line in archivocsv:
+        line = line.strip()
+        columns = line.split("  ")
+
+        if len(columns) >= 3:
+            value = float(columns[2])
+            print(f'{value}\t{line}')
